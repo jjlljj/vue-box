d8ac8dfc920b01dc6ea1fd2a8ae785619a8d8216
James Logue <logueja@gmail.com>
2018-03-26 16:42:15 -0600

    Add initial components

---
+++ src/components/IdeaForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form
+    v-on:submit="submitIdea()"
+    >
+    <h1><span>Vue</span>box</h1>
+    <input
+      type="text"
+      />
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'IdeaForm',
+    props: {
+      submitIdea: Function
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
